d1205826b1cadce3b428997f2f2a62fd2e6b8eea
mehmet <mehmetishaktas@hotmail.com>
2024-11-24 00:52:30 +0300

    deneme1

---
+++ surelervedualar/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace surelervedualar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ surelervedualar/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace surelervedualar
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
